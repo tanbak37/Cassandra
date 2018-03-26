ac75b8c7e4f01f49e7922ee88d49cfe9a531dfc7
tanbak37 <jimmy.dbajo@gmail.com>
2018-03-26 12:25:58 +0700

    baru sampai Acces_token

--- src/main/java/com/dpp/gn/utilities/KeyConstants.java
+++ src/main/java/com/dpp/gn/utilities/KeyConstants.java
@@ -6,6 +6,11 @@ public interface KeyConstants {
 	static final String ACCESS_TOKEN_FB = "EAACEdEose0cBAJJ0uzZCprVxjzE6L8YKmbmNuh9Xz4LgyzZA6ZCQaJWDraAWwAZCOv6QEoQRAfZAa9Ul4o7TuE5pRWPTZC9nZAy3bf9XDgLAZCgiGUCCcBgHTfEEDpVjNkHefeO3I3pFfy59HV0nXqzBsWltLZC4sPRgAqqGyIWpocB3XltXbSSw5BF9lHn5aLRAZD";
 	static final String CONSUMER_KEY_STR = "CPmLmjUTjTunphmskTw7kI0kc";
 	
+	public static final String MY_ACCESS_TOKEN = "EAACEdEose0cBALKZCupvctV23CujRV7zIPJ2QvFmbr9zT3MgDwuZBAR4bSYpzPFm2dPOCapt2IYHFf7qoyghzZBypDmJcTw28Pv24H8JrtSRv24AqFXf8aLyZBhF5ALcx9d83iq7BlXioeZCVU4VGQ2w9Rc1QOGxKPvFhqtT2OYz0prIprcunN83eMbiw7ZC8ZD";
+	 
+	   // Facebook App
+	   public static final String MY_APP_ID = "594652347562923";
+	  // public static final String MY_APP_SECRET = "<your app secret>";
 	
 	static final int RESPONSE_SUCCESS = 200;
 	static final int RESPONSE_FAILED1 = 404;
